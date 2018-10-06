498f96fec0ada07c93b97aee2fd32e49691308b4
nettravel99 <nettravel99@aol.com>
2018-10-05 22:29:55 -0400

    Added reading JWT fropm local storage

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ import {Provider} from "react-redux";
 import store from "./redux/store";
 import Menu from "./components/Menu";
 import App from "./App";
+import {userLoggedIn} from './actions/auth'
+
+
+
+
 
 import {
   BrowserRouter, StaticRouter, // for server rendering
@@ -16,6 +21,17 @@ import {
   Link
   // etc.
 } from "react-router-dom";
+
+
+
+if (localStorage.UserJWT){
+
+  const user  = { token: localStorage.UserJWT}
+  store.dispatch(userLoggedIn(user))
+
+}
+
+
 
 const Hello = () => {
   return <div>Hello</div>;
